37d58c649ca7cb625047a429ee9b332ace69deca
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-06-21 16:18:38 +0200

    start

---
+++ TyroBot/src/main/java/fr/tyrolium/maxime/bot/Main.java
@@ -0,0 +1,23 @@
+package fr.tyrolium.maxime.bot;
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+
+    public static void main(String[] argv) throws LoginException {
+        System.out.println("Lancement");
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        builder.setToken(argv[0]);
+        builder.addEventListeners(new message());
+        builder.setActivity(Activity.playing("tyrolium.fr | Prefix = t! | En Developpement"));
+        builder.build();
+
+        System.out.println("Le bot est bien lance");
+    }
+
+    public static String Prefix = "t!";
+}
